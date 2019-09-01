34b03fa5ea4771a0cfd42212bc4c26665e13b943
Sumiki Ohsawa <sumikio@gmail.com>
2019-09-01 16:50:41 -0700

    circle added

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Circle1 from './components/Circle1.vue'
+import Circle2 from './components/Circle2.vue'
 import App from './App.vue'
 
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
+const routes = [
+    { path: '/', component: Circle1 },
+    { path: '/circle1', component: Circle1 },
+    { path: '/circle2', component: Circle2 },
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    routes
+})
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
